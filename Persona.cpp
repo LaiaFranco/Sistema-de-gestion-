bd4d98c1c7bfdc697638e377e138fac0f6214975
LaiaFranco <deniseef.9@gmail.com>
2025-06-05 18:18:13 -0300

    Add files via upload

--- Persona.cpp
+++ Persona.cpp
@@ -8,20 +8,22 @@ Persona::Persona():_fechaNac(1,1,2023){
         _numDocumento[0] = '\0';
         _nombres[0]= '\0';
         _apellidos[0]= '\0';
-        _numTelefono[0]= '\0';;
+        _numTelefono[0]= '\0';
         _direccion[0]= '\0';
         _mail[0]= '\0';
-
+        _estado = false;
 }
 
-Persona::Persona(int  tipoDocumento, string numDocumento,string nombres, string apellidos, string numTelefono, string direccion,string mail, const Fecha& fechaNac){
+Persona::Persona(int  tipoDocumento, string numDocumento,string nombres, string apellidos,int edad,string numTelefono, string direccion,string mail,bool estado, const Fecha& fechaNac){
     setTipoDocumento(tipoDocumento);
     setNumDocumento(numDocumento);
     setNombres(nombres);
     setApellidos(apellidos);
+    setEdad(edad);
     setNumTelefono(numTelefono);
     setDireccion(direccion);
     setMail(mail);
+    setEstado(estado);
     setFechaNac(fechaNac);
 }
 
@@ -30,7 +32,9 @@ void Persona::setTipoDocumento(int tipoDocumento){
 }
 
 void Persona::setNumDocumento(string numDocumento){
-    strcpy(_numDocumento,numDocumento.c_str());
+   if(numDocumento.size()<=8){
+   strcpy(_numDocumento,numDocumento.c_str());
+   }
 }
 
 void Persona::setNombres(string nombres){
@@ -40,9 +44,13 @@ void Persona::setNombres(string nombres){
 void Persona::setApellidos (string apellidos){
     strcpy(_apellidos,apellidos.c_str());
 }
+
+void Persona::setEdad(int edad){
+    _edad = edad;
+}
+
 void Persona::setNumTelefono (string numTelefono){
         strcpy(_numTelefono, numTelefono.c_str());
-
 }
 
 void Persona::setDireccion(string direccion){
@@ -53,20 +61,81 @@ void Persona::setDireccion(string direccion){
 void Persona::setMail (string mail){
         strcpy(_mail,mail.c_str());
 }
+void Persona::setEstado(bool estado){
+    _estado = estado;
+}
+
+
 void Persona::setFechaNac(const Fecha& fechaNac){
     _fechaNac = fechaNac;
 }
 
-    string getTipoDocumento();
-    string getNumeroDocumento();
-    string getNombres();
-    string getApellidos();
-    string getNumTelefono();
-    string getDireccion();
-    string getMail();
-    Fecha getFechaNac();
+int Persona::getTipoDocumento(){
+    return _tipoDocumento;
+}
 
+string Persona::getNumeroDocumento(){
+    return _numDocumento;
+}
+string Persona::getNombres(){
+    return _nombres;
+}
+string Persona::getApellidos(){
+    return _apellidos;
+}
+int Persona::getEdad(){
+    return _edad;
+}
+string Persona::getNumTelefono(){
+    return _numTelefono;
+}
+string Persona::getDireccion(){
+    return _direccion;
+}
+string Persona::getMail(){
+    return _mail;
+}
+bool Persona::getEstado(){
+    return _estado;
+}
+Fecha getFechaNac(){
+    return _fechaNac;
+}
 
+void Persona::Cargar(){
+    cout<<"-----DATOS PERSONALES-----"<<endl;
+    cout<<"TIPO DE DOCUMENTO(1-DNI, 2-PASAPORTE, 3-LIBRETA CIVICA): ";
+    cin>>_tipoDocumento;
+    cout<<endl;
+    cout<<"NUMERO: ";
+    cin>>_numDocumento;
+    cout>>endl;
+    cout<<"NOMBRES: ";
+    cin.ignore();
+    cin.getline(_nombres,50);
+    cout<<endl;
+    cout<<"APELLIDOS: ";
+    cin.ignore();
+    cin.getline(_apellidos,50);
+    cout<<endl;
+    cout<<"EDAD: ";
+    cin>>_edad;
+    cout<<endl;
+    cout<<"NUMERO DE TELEFONO: " ;
+    cin.ignore();
+    cin.getline(_numTelefono,15);
+    cout<<endl;
+    cout<<"DIRECCION: ";
+    cin.ignore();
+    cin.getline(_direccion,20);
+    cout<<endl;
+    cout<<"MAIL: ";
+    cin.ignore();
+    cin.getline(_mail,50);
+    cout<<endl;
+    _estado = true;
+
+}
 
 
 
--- Persona.h
+++ Persona.h
@@ -8,35 +8,43 @@ class Persona{
 
 public:
     Persona();
-    Persona(int tipoDocumento, string numDocumento,string nombres, string apellidos, string numTelefono, string direccion,string mail, const Fecha& fechaNac);
+    Persona(int tipoDocumento, string numDocumento,string nombres, string apellidos,int edad, string numTelefono, string direccion,string mail, const Fecha& fechaNac);
 
     void setTipoDocumento(int  tipoDocumento);
     void setNumDocumento(string numDocumento);
     void setNombres(string nombres);
     void setApellidos (string apellidos);
+    void setEdad(int edad);
     void setNumTelefono (string numTelefono);
     void setDireccion(string direccion);
     void setMail (string mail);
+    void setEstado(bool estado);
     void setFechaNac(const Fecha& fechaNac);
 
     int  getTipoDocumento();
     string getNumeroDocumento();
     string getNombres();
     string getApellidos();
+    int getEdad();
     string getNumTelefono();
     string getDireccion();
     string getMail();
+    bool getEstado();
     Fecha getFechaNac();
 
+    void Mostrar();
+    void Cargar();
 
 private:
     int  _tipoDocumento;
     char _numDocumento [8];
     char _nombres [50];
     char _apellidos[50];
+    int _edad;
     char _numTelefono [15];
     char _direccion[20];
     char _mail[50];
+    bool _estado;
     Fecha _fechaNac;
 
 
