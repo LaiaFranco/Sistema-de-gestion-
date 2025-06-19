0786bece5abfd75de7fa32df817948211ae2ff4b
Valentina2093 <valuvale23@gmail.com>
2025-06-19 13:11:31 -0300

    ArchivoProfesoresxCurso.h

--- ArchivoProfesoresxCurso.h
+++ ArchivoProfesoresxCurso.h
@@ -9,23 +9,23 @@ class ArchivoProfesoresxCurso{
 
 
  public:
-     
+
      ArchivoProfesoresxCurso(char const *nombre ="profesoresxcurso.dat");
-     
-     
+
+
     int CantidadRegistro();
     bool agregarRelacion(ProfesoresxCurso reg);
     void listarRegistroTotal();
     void listarPorProfesor(int legajoProfesor);
     void listarPorCurso(int  idCurso);
-    void buscarRelacion(ProfesoresxCurso reg);
-    void modificarRegistro(ProfesoresxCurso reg);
+    int buscarRelacion( ProfesoresxCurso reg);
+    bool modificarRegistro(ProfesoresxCurso reg);
     bool bajaLogica(int  legajoProfesor, int  idCurso );
     bool altaLogica(int legajoProfesor, int idCurso);
     ProfesoresxCurso leerProfesoresxCurso(int pos);
-   
 
-    
+
+
     private:
       char _nombreArchivo[30];
       int _tamanioRegistro;
